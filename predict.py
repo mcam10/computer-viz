afc029b23a3a1a9fd44ba86f55afe3995c99d34c
Malik Cameron <malikcameron95@gmail.com>
2024-10-11 15:57:59 -0700

    Matplotlib plotting

--- predict.py
+++ predict.py
@@ -12,7 +12,7 @@ import numpy as np
 model = YOLO("yolov8m-seg.pt")  # load an official model
 
 #Add img or Video as a var to pass to cv2
-img = cv2.imread("/home/gear/yolo/20240904140832_MP4-0016.jpg")
+img = cv2.imread("/Users/mcameron/yolo/20240904140832_MP4-0016.jpg")
 
 # Saves to /runs/predict/img
 #model.predict(img, save=True)
@@ -26,35 +26,12 @@ person_points = {}
 person = results[0].masks.xy[0]
 ball = results[0].masks.xy[8]
 
-#print(person)
-#print(ball)
-plt.imshow(person)
-plt.colorbar()
+#plotting
+
+plt.title("Line graph")
+plt.xlabel("X axis")
+plt.ylabel("Y axis")
+plt.plot(*zip(*person), marker='o', color='r', ls='')
+plt.plot(*zip(*ball), marker='o', color='b', ls='')
+
 plt.show()
-
-#next i need to figure out whats the best representation for a intersection of two points aka player and ball
-## either masks or boxes classes respectively
-
-
-#we have the box of the ball
-#print(results[0].boxes.xyxy[0])
-
-##(results[0].boxes.xyxy[8])
-
-
-#for idx,point in enumerate(person):
-#    print(idx,point)
-
-#print(results[0])
-#xy_cords = masks.xy
-
-#print(len(xy_cords))  # Number of masks
-#print(xy_cords[0].shape)
-
-
-# Convert results to a list of dictionaries and then to a DataFrame
-#data = [{'xmin': box[0], 'ymin': box[1], 'xmax': box[2], 'ymax': box[3],
-#         'confidence': box[4], 'class': box[5]} for box in results[0].masks.xyn]
-#df = pd.DataFrame(data)
-#print(df)
-
--- visuals.py
+++ visuals.py
@@ -3,6 +3,7 @@ import cv2
 import torch
 import pandas as pd
 import numpy as np
+from pathlib import Path
 
 # Load a model
 #model = YOLO("yolov8m-pose.pt")  # load an official model
@@ -12,34 +13,45 @@ model = YOLO("yolov8m-seg.pt")  # load an official model
 #Add img or Video as a var to pass to cv2
 img = cv2.imread("/Users/mcameron/yolo/20240904140832_MP4-0016.jpg")
 
-results = model(img)
+#results = model.predict(img)
+results = model.predict(img, save_crop=True)
 
-results[0].show()
+for result in results:
+    orig = np.copy(result.orig_img)
+    img_name = Path(result.path).stem
 
-df_result = results[0].to_df()
+    #Iterate each pbject contour
+    for ci, c in enumerate(result):
+        label = c.names[c.boxes.cls.tolist().pop()]
 
-print(df_result.loc[[0,8]])
+        b_mask = np.zeros(orig.shape[:2], np.uint8)
+
+        #create contour mask
+
+        contour = c.masks.xy.pop().astype(np.int32).reshape(-1,1,2)
+        _ = cv2.drawContours(b_mask, [contour], -2, (255, 255, 255), cv2.FILLED)
+
+        mask3ch = cv2.cvtColor(b_mask, cv2.COLOR_GRAY2BGR)
+        isolated = cv2.bitwise_and(mask3ch, orig)
 
 
-numpy_arr = np.copy(results[0].orig_img) 
-b_mask = np.zeros(numpy_arr.shape[:2], np.uint8)
-contour = results[0].masks.xy[0].astype(np.int32).reshape(-1, 1, 2)
-_ = cv2.drawContours(b_mask, [contour], -1, (255, 255, 255), cv2.FILLED)
+        _ = cv2.imwrite(f"{img_name}_{label}-{ci}.png", isolated)
 
-## isolate the object using the binary mask
-mask3ch = cv2.cvtColor(b_mask, cv2.COLOR_GRAY2BGR)
-isolated = cv2.bitwise_and(mask3ch, numpy_arr)
 
-cv2.imwrite(f"{numpy_arr}.png", isolated)
 
-# Convert results to a list of dictionaries and then to a DataFrame
-#data = [{'xmin': box[0], 'ymin': box[1], 'xmax': box[2], 'ymax': box[3],
-#         'confidence': box[4], 'class': box[5]} for box in results[0].masks.xyn]
-#df = pd.DataFrame(data)
-#print(df)
+#df_result = results[0].to_df()
 
-#for cords,elem in enumerate(results[0].masks.xyn):
-#    print(cords, elem)
+#print(df_result.loc[[0,8]])
+
+#print(results.masks)
+
+#orig = np.copy(results[0].orig_img)
+#binary_mask = np.zeros(orig.shape[:2],np.uint8)
+#contour = results[0].masks.xy[0].astype(np.int32).reshape(-1,1,2)
+#cv2.drawContours(binary_mask, [contour], -1, (255, 255, 255), cv2.FILLED)
+
+#cv2.imshow('binary_mask', binary_mask)
+
 
 #results.print()
 ## Get specific keypoint of leg and ball and see how we can track touches
